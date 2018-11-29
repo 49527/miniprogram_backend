0894e1f88edd1687c2e796d23e4521460dfb5e0e
Hangyu Fan <fhy14@mails.tsinghua.edu.cn>
2018-11-29 09:55:20 +0800

    description for state_choice

--- ordersys/choices/state_choices.py
+++ ordersys/choices/state_choices.py
@@ -1,12 +1,15 @@
+# coding=UTF-8
+from __future__ import unicode_literals
+from django.utils.translation import ugettext_lazy as _
 from base.util.field_choice import FieldChoice
 
 
 class _OrderStateChoice(FieldChoice):
     CHOICE_DISPLAY = (
-        (0, "", "CREATED"),
-        (1, "", "ACCEPTED"),
-        (2, "", "CANCELED"),
-        (3, "", "COMPLETED"),
+        (0, _("创建"), "CREATED"),
+        (1, _("已接单"), "ACCEPTED"),
+        (2, _("被取消"), "CANCELED"),
+        (3, _("已完成"), "COMPLETED"),
     )
 
 
--- ordersys/models/order.py
+++ ordersys/models/order.py
@@ -9,7 +9,7 @@ from usersys.models import UserBase
 class OrderInfo(models.Model):
     create_time = models.DateTimeField(_('订单创建时间'), auto_now_add=True)
     location = models.CharField(max_length=100)
-    amount = models.FloatField(_("订单金额"))
+    amount = models.FloatField(_("订单金额"), default=0.0)
     o_state = models.IntegerField(_("订单状态"), choices=order_state_choice.choice)
     uid = models.ForeignKey(
         UserBase,
