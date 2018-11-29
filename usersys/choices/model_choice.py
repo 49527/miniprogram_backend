be2cc1b163c07ae8939cc64b4ef22e378e48f404
xss <xssstory@gmail.com>
2018-11-29 18:38:42 +0800

    update model_choice.py

--- usersys/choices/model_choice.py
+++ usersys/choices/model_choice.py
@@ -1,13 +1,14 @@
 # coding=UTF-8
+from django.utils.translation import ugettext_lazy as _
 from base.util.field_choice import FieldChoice
 
 
 class _ValidateStatusChoice(FieldChoice):
     CHOICE_DISPLAY = (
-        (0, "", "NOT_COMMITTED"),
-        (1, "", "NOT_PROCEEDED"),
-        (2, "", "ACCEPTED"),
-        (3, "", "REJECTED"),
+        (0, _("用户尚未提交认证审核申请"), "NOT_COMMITTED"),
+        (1, _("用户已提交认证审核申请，但审核尚未被处理"), "NOT_PROCEEDED"),
+        (2, _("用户已提交认证审核申请，且审核已通过"), "ACCEPTED"),
+        (3, _("用户已提交认证审核申请，但审核被拒绝"), "REJECTED"),
     )
 
 
