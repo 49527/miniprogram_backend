9ab87637a6dd63c866baa7fa00837781282c1346
Hangyu Fan <fhy14@mails.tsinghua.edu.cn>
2018-11-29 09:50:21 +0800

    Choice field description for transaction choice

--- walletsys/choice/transaction_choice.py
+++ walletsys/choice/transaction_choice.py
@@ -1,12 +1,16 @@
+# coding=UTF-8
+from __future__ import unicode_literals
+from django.utils.translation import ugettext_lazy as _
 from base.util.field_choice import FieldChoice
 
 
 class _TransactionChoice(FieldChoice):
     CHOICE_DISPLAY = (
-        (0, "", "COLLECT_MONEY"),
-        (1, "", "APPLY"),
-        (2, "", "GET_MONEY_SUCCESS"),
-        (3, "", "GET_MONEY_FAILURE")
+        (0, _("收款"), "COLLECT_MONEY"),
+        (1, _("申请提现"), "APPLY"),
+        (2, _("提现成功"), "GET_MONEY_SUCCESS"),
+        (3, _("提现失败"), "GET_MONEY_FAILURE")
     )
+
 
 transaction_type = _TransactionChoice()
