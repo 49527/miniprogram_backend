bbde67c3cdab72de2244c65fd24148c700b10807
Hangyu Fan <fhy14@mails.tsinghua.edu.cn>
2018-12-03 08:42:53 +0800

    bugfix: order sys return 500 when call obtain api

--- ordersys/funcs/obtain.py
+++ ordersys/funcs/obtain.py
@@ -1,15 +1,27 @@
 from usersys.funcs.utils.usersid import user_from_sid
 from base.exceptions import Error404, WLException
 from usersys.models import UserBase
+from usersys.choices.model_choice import user_role_choice
 from base.util.pages import get_page_info
 from django.db import models
 from ordersys.choices.state_choices import order_state_choice
 
 
+def get_user_order_queryset(user):
+    if user.role == user_role_choice.CLIENT:
+        qs = user.order_c
+    elif user.role == user_role_choice.RECYCLING_STAFF:
+        qs = user.order_b
+    else:
+        raise AssertionError("user's role is neither CLIENT nor RECYCLER")
+
+    return qs
+
+
 @user_from_sid(Error404)
 def obtain_order_list(user, page, count_per_page):
     # type: (UserBase, int, int) -> (QuerySet, int)
-    qs = user.order
+    qs = get_user_order_queryset(user)
     start, end, n_pages = get_page_info(
         qs, count_per_page, page,
         index_error_excepiton=WLException(400, "Page out of range")
@@ -21,7 +33,7 @@ def obtain_order_list(user, page, count_per_page):
 @user_from_sid(Error404)
 def obtain_overview(user):
     # type: (UserBase) -> (int, float)
-    orders = user.order
+    orders = get_user_order_queryset(user)
     n_times = orders.count()
     total_amount = orders.filter(o_state=order_state_choice.COMPLETED).aggregate(models.Sum('amount'))["amount__sum"]
     total_amount = 0 if total_amount is None else total_amount
