661c2836415e966566fda5b050217f988d704054
xss <xssstory@gmail.com>
2018-12-04 16:28:16 +0800

    update user info

--- usersys/funcs/obtain.py
+++ usersys/funcs/obtain.py
@@ -1,8 +1,8 @@
 from usersys.funcs.utils.usersid import user_from_sid
 from base.exceptions import Error404
-from usersys.choices.state_choice import is_validate_choice
 from usersys.choices.model_choice import user_validate_status
 from usersys.models import UserBase, UserValidate
+from ordersys.funcs.obtain import obtain_overview
 
 
 @user_from_sid(Error404)
@@ -10,8 +10,8 @@ def obtain_self_info(user):
     # type: (UserBase) -> (UserBase, int)
     try:
         validate = user.user_validate
-        state = is_validate_choice.VALIDATED if validate.validate_status == user_validate_status.ACCEPTED \
-            else is_validate_choice.NOT_VALIDATED
+        is_validated = True if validate.validate_status == user_validate_status.ACCEPTED else False
     except UserValidate.DoesNotExist:
-        state = is_validate_choice.NOT_VALIDATED
-    return user, state
+        is_validated = False
+    n_times, total_amount = obtain_overview(user=user)
+    return user, is_validated, n_times, total_amount
