293f47cd94b2c3d8f3ddeeec5e86d6907b552d28
Hangyu Fan <fhy14@mails.tsinghua.edu.cn>
2018-12-08 10:34:29 +0800

    admin site optimized for top type

--- category_sys/admin.py
+++ category_sys/admin.py
@@ -3,19 +3,40 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from category_sys.models import ProductSubType, ProductTopType
+from category_sys.choices.model_choices import top_type_choice
 
 
 # Register your models here.
 
-class SubTypeInline(admin.TabularInline):
+class SubTypeInlineC(admin.TabularInline):
     model = ProductSubType
+    extra = 1
+    fk_name = "toptype_c"
+
+
+class SubTypeInlineB(admin.TabularInline):
+    model = ProductSubType
+    extra = 1
+    fk_name = "toptype_b"
 
 
 class TopTypeAdmin(admin.ModelAdmin):
-    list_display = ('t_top_name', 'operator')
+    list_display = ('t_top_name', 'operator', 'in_use')
     list_editable = ('in_use', )
     list_filter = ('operator', )
-    inlines = (SubTypeInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        # type: (object, ProductTopType) -> object
+
+        # Dynamic inline based on top type operator
+        if obj is not None:
+            if obj.operator == top_type_choice.CONSUMER:
+                self.inlines = [SubTypeInlineC]
+
+            if obj.operator == top_type_choice.BUSINESS:
+                self.inlines = [SubTypeInlineB]
+
+        return super(TopTypeAdmin, self).get_inline_instances(request, obj)
 
 
 admin.site.register(ProductTopType, TopTypeAdmin)
