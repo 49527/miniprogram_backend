656e1e62e41b5bd4354709f8eb5c1f2c0452b836
Hangyu Fan <fhy14@mails.tsinghua.edu.cn>
2018-12-09 18:08:59 +0800

    feature: treed price of categories in business system.

--- business_sys/serializers/recycle_bin.py
+++ business_sys/serializers/recycle_bin.py
@@ -1,31 +1,30 @@
-from django.db.models import Min, Max
+from django.db.models import Min, Max, Prefetch
 from rest_framework import serializers
-from business_sys.models import RecycleBin
-from category_sys.models import ProductTopType
+from business_sys.models import RecycleBin, BusinessProductTypeBind
+from category_sys.models import ProductTopType, ProductSubType
 from category_sys.choices.model_choices import top_type_choice
+from business_sys.serializers.recycle_product import BusinessProductTopTypeSerializer
 
 
 class RecycleBinDisplaySerializer(serializers.ModelSerializer):
-    c_type = serializers.ReadOnlyField(source="product_subtype.")
 
     class Meta:
         model = RecycleBin
         fields = (
-            "id", "GPS_L", "GPS_A", "rb_name", "r_b_type", "loc_desc", "pn", "c_type"
+            "id", "GPS_L", "GPS_A", "rb_name", "r_b_type", "loc_desc", "pn"
         )
 
     def to_representation(self, instance):
         data = super(RecycleBinDisplaySerializer, self).to_representation(instance)
         type_list = []
-        recycle_bin = RecycleBin.objects.get(id=data["id"])
         for c_type in ProductTopType.objects.filter(operator=top_type_choice.CONSUMER, in_use=True):
             dic = {
                 "type_id": c_type.id,
                 "c_type": c_type.t_top_name,
-                "min_price": recycle_bin.product_subtype.filter(
+                "min_price": instance.product_subtype.filter(
                     p_type__toptype_c=c_type,
                     p_type__in_use=True).aggregate(Min("price"))["price__min"],
-                "max_price": recycle_bin.product_subtype.filter(
+                "max_price": instance.product_subtype.filter(
                     p_type__toptype_c=c_type,
                     p_type__in_use=True).aggregate(Max("price"))["price__max"]
             }
@@ -34,6 +33,42 @@ class RecycleBinDisplaySerializer(serializers.ModelSerializer):
         return data
 
 
+class RecycleBinBusinessPriceDisplaySerializer(serializers.ModelSerializer):
+
+    last_update = serializers.SerializerMethodField()
+    categories = serializers.SerializerMethodField()
+
+    class Meta:
+        model = RecycleBin
+        fields = (
+            "id", "rb_name", "r_b_type", "loc_desc", "pn", "last_update", "categories"
+        )
+
+    def get_last_update(self, obj):
+        # type: (RecycleBin) -> int
+        return obj.product_subtype.aggregate(Max("modified_time"))["modified_time__max"]
+
+    def get_categories(self, obj):
+        queryset = ProductTopType.objects.filter(operator=top_type_choice.BUSINESS).prefetch_related(
+            Prefetch(
+                'b_subtype',
+                queryset=ProductSubType.objects.filter(in_use=True).prefetch_related(
+                    Prefetch(
+                        'business',
+                        queryset=BusinessProductTypeBind.objects.filter(recycle_bin=obj),
+                        to_attr='business_bind')
+                ),
+                to_attr='pst_prefetch')
+        )
+        return BusinessProductTopTypeSerializer(queryset, many=True).data
+
+
 class NearbyDisplaySerializer(serializers.Serializer):
     recycle_bin = RecycleBinDisplaySerializer()
     distance = serializers.FloatField()
+
+
+def test():
+    rb1 = RecycleBin.objects.first()
+    dt = RecycleBinBusinessPriceDisplaySerializer(rb1).data
+    print dt
