a9c27fb2f16ecc2a5d8a35e0b501793ac1fe9b2a
Hangyu Fan <fhy14@mails.tsinghua.edu.cn>
2018-12-13 15:14:35 +0800

    urls

--- business_sys/urls.py
+++ business_sys/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from business_sys.views import obtain, operate, user_center
+from business_sys.views import obtain, operate, user_center, truck
 
 
 obtain_urls =[
@@ -13,11 +13,17 @@ operate_urls = [
     url(r'^product_price_update/$', operate.CategoryPriceUpdateView.as_view()),
 ]
 
+truck_urls = [
+    url(r'^obtain_truck_info/$', truck.ObtainTruckOrderInfoView.as_view()),
+    url(r'^generate_loading_credential/$', truck.CreateTruckOrderInfoView.as_view()),
+]
+
 
 b_urls = [
     url(r'^user_center/$', user_center.ObtainRecyclingStaffInfoView.as_view()),
     url(r'^rb_product_detail/$', obtain.ObtainRecycleBinPriceListView.as_view()),
     url(r'^upload_gps/$', user_center.UploadGps.as_view()),
+    url(r'^truck/$', include(truck_urls)),
 ]
 
 
