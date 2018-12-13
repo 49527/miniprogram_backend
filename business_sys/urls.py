06f00de96c427ffda979e654f457ba362811cc9b
Hangyu Fan <fhy14@mails.tsinghua.edu.cn>
2018-12-13 15:19:55 +0800

    url - bugfix

--- business_sys/urls.py
+++ business_sys/urls.py
@@ -23,7 +23,7 @@ b_urls = [
     url(r'^user_center/$', user_center.ObtainRecyclingStaffInfoView.as_view()),
     url(r'^rb_product_detail/$', obtain.ObtainRecycleBinPriceListView.as_view()),
     url(r'^upload_gps/$', user_center.UploadGps.as_view()),
-    url(r'^truck/$', include(truck_urls)),
+    url(r'^truck/', include(truck_urls)),
 ]
 
 
